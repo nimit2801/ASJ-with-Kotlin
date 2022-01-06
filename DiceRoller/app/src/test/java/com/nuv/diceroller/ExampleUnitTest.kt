391aa30a31e1574b63075b96d0b2875d0901b5b1
nimit2801 <mnimitsavant@outlook.com>
2022-01-07 05:03:59 +0530

    Dice Roller Complete ✨

--- DiceRoller/app/src/test/java/com/nuv/diceroller/ExampleUnitTest.kt
+++ DiceRoller/app/src/test/java/com/nuv/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,13 @@
 package com.nuv.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generate_number() {
+        val dice = Dice(6)
+        val rollResults = dice.roll()
+        assertTrue("The value of the result is between 1 and 6", rollResults in 1..6)
     }
 }
